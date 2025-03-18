3c69b4b9c1c5f9c4cd2b00f8490951be3fd929f8
Rime <81419447+Emirlol@users.noreply.github.com>
2025-03-18 09:54:57 +0300

    Add nullable codecs

--- src/main/kotlin/util/CodecUtil.kt
+++ src/main/kotlin/util/CodecUtil.kt
@@ -1,7 +1,12 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package me.rime.rimelib.util
 
-import com.mojang.serialization.Codec
+import com.mojang.datafixers.util.Pair
+import com.mojang.serialization.*
 import com.mojang.serialization.codecs.RecordCodecBuilder
+import java.util.*
+import java.util.stream.Stream
 
 fun <A, T> dataClassCodecBuilder(constructor: (A) -> T, codec: RecordCodecBuilder<T, A>): Codec<T> = RecordCodecBuilder.create {
 	it.group(codec).apply(it, constructor)
@@ -66,3 +71,47 @@ fun <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, T> dataClassCodecBuilder(const
 fun <A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, T> dataClassCodecBuilder(constructor: (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P) -> T, codec: RecordCodecBuilder<T, A>, codec2: RecordCodecBuilder<T, B>, codec3: RecordCodecBuilder<T, C>, codec4: RecordCodecBuilder<T, D>, codec5: RecordCodecBuilder<T, E>, codec6: RecordCodecBuilder<T, F>, codec7: RecordCodecBuilder<T, G>, codec8: RecordCodecBuilder<T, H>, codec9: RecordCodecBuilder<T, I>, codec10: RecordCodecBuilder<T, J>, codec11: RecordCodecBuilder<T, K>, codec12: RecordCodecBuilder<T, L>, codec13: RecordCodecBuilder<T, M>, codec14: RecordCodecBuilder<T, N>, codec15: RecordCodecBuilder<T, O>, codec16: RecordCodecBuilder<T, P>): Codec<T> = RecordCodecBuilder.create {
 	it.group(codec, codec2, codec3, codec4, codec5, codec6, codec7, codec8, codec9, codec10, codec11, codec12, codec13, codec14, codec15, codec16).apply(it, constructor)
 }
+
+inline fun <A> Codec<A>.ofNullable() = NullableCodec(this)
+
+/**
+ * A codec that allows null values to be encoded and decoded rather than creating [Optional] instances.
+ *
+ * @param codec The codec to wrap.
+ */
+data class NullableCodec<A>(private val codec: Codec<A>) : Codec<A?> {
+	override fun <T : Any?> encode(input: A?, ops: DynamicOps<T>, prefix: T): DataResult<T> = when (input) {
+		null -> DataResult.success(ops.empty())
+
+		else -> codec.encode(input, ops, prefix)
+	}
+
+	@Suppress("KotlinConstantConditions")
+	override fun <T : Any?> decode(ops: DynamicOps<T>, input: T): DataResult<Pair<A?, T>> = when (input) {
+		null -> DataResult.success(Pair.of(null, input))
+
+		else -> codec.decode(ops, input)
+	}
+}
+
+inline fun <A : Any> Codec<A>.nullableFieldOf(name: String, lenient: Boolean = false) = NullableFieldCodec(name, this, lenient)
+
+data class NullableFieldCodec<A : Any>(private val name: String, private val codec: Codec<A>, private val lenient: Boolean) : MapCodec<A?>() {
+	override fun <T : Any?> keys(ops: DynamicOps<T>): Stream<T> = Stream.of(ops.createString(name))
+
+	override fun <T> decode(ops: DynamicOps<T>, input: MapLike<T>): DataResult<A?> {
+		val value: T = input.get(name) ?: return DataResult.success(null)
+		val parsed = codec.parse(ops, value)
+		return when {
+			parsed.isError && lenient -> DataResult.success(null)
+			else -> parsed
+		}
+	}
+
+	override fun <T : Any?> encode(input: A?, ops: DynamicOps<T>, prefix: RecordBuilder<T>): RecordBuilder<T> = when (input) {
+		null -> prefix
+
+		else -> prefix.add(name, codec.encodeStart(ops, input))
+	}
+}
+
