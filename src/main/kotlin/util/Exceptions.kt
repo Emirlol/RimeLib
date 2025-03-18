b19857189a1183fa532279f95aa51193ac11c61c
Rime <81419447+Emirlol@users.noreply.github.com>
2025-03-18 10:01:52 +0300

    Add Exceptions.kt for exception utilities

---
+++ src/main/kotlin/util/Exceptions.kt
@@ -0,0 +1,17 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package me.rime.rimelib.util
+
+inline fun unsupported(): Nothing = throw UnsupportedOperationException()
+
+inline fun unsupported(cause: Throwable): Nothing = throw UnsupportedOperationException(cause)
+
+inline fun unsupported(message: String): Nothing = throw UnsupportedOperationException(message)
+
+inline fun unsupported(message: Any): Nothing = throw UnsupportedOperationException(message.toString())
+
+inline fun unsupported(cause: Throwable, message: String): Nothing = throw UnsupportedOperationException(message, cause)
+
+inline fun unsupported(cause: Throwable, lazyMessage: () -> String): Nothing = throw UnsupportedOperationException(lazyMessage(), cause)
+
+inline fun unsupported(cause: Throwable, message: Any): Nothing = throw UnsupportedOperationException(message.toString(), cause)
