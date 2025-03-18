620fd4be07e511c75e1fbbe7f7eec2da8876ccb8
Rime <81419447+Emirlol@users.noreply.github.com>
2025-03-18 09:58:35 +0300

    Separate initializers for client and server
    This is experimental and I haven't done anything with it really

--- src/main/kotlin/RimeLib.kt
+++ src/main/kotlin/RimeLib.kt
@@ -3,6 +3,14 @@ package me.rime.rimelib
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.cbor.Cbor
 import kotlinx.serialization.json.Json
+import me.rime.rimelib.ui.debug.DebugScreen
+import me.rime.rimelib.util.client
+import me.rime.rimelib.util.fabricLoader
+import me.rime.rimelib.util.register
+import net.fabricmc.api.EnvType
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents.ServerStarting
 import net.minecraft.util.Identifier
 
 @OptIn(ExperimentalSerializationApi::class)
@@ -10,6 +18,27 @@ object RimeLib {
 	@Deprecated("This method is called by the Fabric Loader. Do not call this method manually.", level = DeprecationLevel.ERROR)
 	fun init() {
 //		Initializer // Initialize features.
+		when (fabricLoader.environmentType) {
+			EnvType.CLIENT -> initClient()
+			EnvType.SERVER -> initServer()
+			null -> throw IllegalStateException("Environment type is null")
+		}
+	}
+
+	private fun initClient() {
+		ClientCommandRegistrationCallback.EVENT.register(NAMESPACE) {
+			literal("openScreen") {
+				executes {
+					client.send {
+						client.setScreen(DebugScreen())
+					}
+				}
+			}
+		}
+	}
+
+	private fun initServer() {
+
 	}
 
 	fun identifier(path: String): Identifier = Identifier.of(NAMESPACE, path)
