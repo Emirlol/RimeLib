f399123a5f89448b019ea1a3f64ed00477bbc185
Rime <81419447+Emirlol@users.noreply.github.com>
2025-06-20 16:59:02 +0300

    Remove nonsense test code

--- src/main/kotlin/RimeLib.kt
+++ src/main/kotlin/RimeLib.kt
@@ -1,19 +1,12 @@
 package me.ancientri.rimelib
 
 import me.ancientri.rimelib.util.LoggerFactory
-import me.ancientri.rimelib.util.command.register
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.minecraft.util.Identifier
 
 object RimeLib {
 	@Deprecated("This method is called by the Fabric Loader. Do not call this method manually.", level = DeprecationLevel.ERROR, replaceWith = ReplaceWith(""))
 	fun init() {
 		Initializer // Initialize features.
-		CommandRegistrationCallback.EVENT.register(NAMESPACE) {
-			literal("asd") executes {
-				it.source
-			}
-		}
 	}
 
 	fun identifier(path: String): Identifier = Identifier.of(NAMESPACE, path)
