06c1bf252b83af6517f49374b97770b20fdcd971
Rime <81419447+Emirlol@users.noreply.github.com>
2025-06-20 16:57:26 +0300

    Delete ui code after merge to keep it contained within the ui branch

--- src/main/kotlin/ui/base/Constants.kt
+++
@@ -1,7 +0,0 @@
-package me.ancientri.rimelib.ui.base
-
-object Constants {
-	const val LEFT_MOUSE_BUTTON = 0
-	const val RIGHT_MOUSE_BUTTON = 1
-	const val MIDDLE_MOUSE_BUTTON = 2
-}
--- src/main/kotlin/ui/base/Positioning.kt
+++
@@ -1,6 +0,0 @@
-package me.ancientri.rimelib.ui.base
-
-enum class Positioning {
-	ABSOLUTE,
-	RELATIVE
-}
