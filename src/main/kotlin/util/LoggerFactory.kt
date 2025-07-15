be74a127fed9425fdd530881c6ce1548d339a181
Rime <81419447+Emirlol@users.noreply.github.com>
2025-07-15 08:40:09 +0300

    Switch to MDC rather than messing with logger names

--- src/main/kotlin/util/LoggerFactory.kt
+++ src/main/kotlin/util/LoggerFactory.kt
@@ -1,11 +1,15 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package me.ancientri.rimelib.util
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.slf4j.MDC
+import java.lang.reflect.Proxy
 import kotlin.reflect.KClass
 
 /**
- * A [slf4j LoggerFactory][org.slf4j.LoggerFactory] wrapper that allows you to create loggers with a specific prefix.
+ * A [slf4j LoggerFactory][org.slf4j.LoggerFactory] wrapper that allows you to create loggers with a specific prefix using MDC (Mapped Diagnostic Context).
  *
  * This is meant to be kept in a single instance, so you can create it once with your mod's name and use it everywhere while keeping it obvious which mod the log is from.
  */
@@ -15,26 +19,47 @@ class LoggerFactory(private val prefix: String) {
 	 *
 	 * @param name The name of the logger.
 	 */
-	fun createLogger(name: String): Logger = LoggerFactory.getLogger("$prefix | $name")
+	fun createLogger(name: String): Logger {
+		val logger = LoggerFactory.getLogger(name)
+		return createMdcProxy(logger)
+	}
 
 	/**
 	 * Creates a logger with the name of the class.
 	 *
 	 * @param clazz The class to create the logger for.
 	 */
-	fun createLogger(clazz: KClass<*>): Logger = createLogger(clazz.simpleName!!)
+	inline fun createLogger(clazz: KClass<*>): Logger = createLogger(clazz.qualifiedName!!)
 
 	/**
 	 * Creates a logger with the name of the class.
 	 *
 	 * @param clazz The class to create the logger for.
 	 */
-	fun createLogger(clazz: Class<*>): Logger = createLogger(clazz.simpleName)
+	inline fun createLogger(clazz: Class<*>): Logger = createLogger(clazz.name)
 
 	/**
 	 * Creates a logger with the name of the class.
 	 *
 	 * @param this The class to create the logger for.
 	 */
-	fun createLogger(`this`: Any): Logger = createLogger(`this`::class.simpleName!!)
+	inline fun createLogger(`this`: Any): Logger = createLogger(`this`::class.qualifiedName!!)
+
+	private fun createMdcProxy(logger: Logger): Logger = Proxy.newProxyInstance(
+		logger.javaClass.classLoader,
+		arrayOf(Logger::class.java)
+	) { _, method, args ->
+		val old = MDC.get(PREFIX_KEY)
+		try {
+			MDC.put(PREFIX_KEY, prefix)
+			method.invoke(logger, *args ?: emptyArray())
+		} finally {
+			if (old != null) MDC.put(PREFIX_KEY, old)
+			else MDC.remove(PREFIX_KEY)
+		}
+	} as Logger
+
+	companion object {
+		const val PREFIX_KEY = "prefix"
+	}
 }
