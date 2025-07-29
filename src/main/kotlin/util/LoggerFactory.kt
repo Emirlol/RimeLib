8ca5021f6adf2d878601385d18fbeab828d2b1d2
Rime <81419447+Emirlol@users.noreply.github.com>
2025-07-29 16:40:32 +0300

    Another workaround for prefixed loggers that allow level modification

--- src/main/kotlin/util/LoggerFactory.kt
+++ src/main/kotlin/util/LoggerFactory.kt
@@ -2,24 +2,29 @@
 
 package me.ancientri.rimelib.util
 
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.core.config.Configurator
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.slf4j.MDC
-import java.lang.reflect.Proxy
 import kotlin.reflect.KClass
 
 /**
- * A [slf4j LoggerFactory][org.slf4j.LoggerFactory] wrapper that allows you to create loggers with a specific prefix using MDC (Mapped Diagnostic Context).
+ * A [slf4j LoggerFactory][org.slf4j.LoggerFactory] wrapper that allows you to create loggers with a specific prefix.
  *
  * This is meant to be kept in a single instance, so you can create it once with your mod's name and use it everywhere while keeping it obvious which mod the log is from.
  */
 class LoggerFactory(private val prefix: String) {
 	/**
 	 * Creates a logger with the given name, prefixed with the provided prefix.
+	 * This logger will be set to DEBUG level in development environments for convenience.
 	 *
 	 * @param name The name of the logger.
 	 */
-	fun createLogger(name: String): Logger = createMdcProxy(LoggerFactory.getLogger(name))
+	fun createLogger(name: String): Logger {
+		val logger = LoggerFactory.getLogger("$prefix | $name")
+		if (FabricLoader.isDevelopmentEnvironment) Configurator.setLevel(logger.name, Level.DEBUG)
+		return logger
+	}
 
 	/**
 	 * Creates a logger with the name of the class.
@@ -41,22 +46,4 @@ class LoggerFactory(private val prefix: String) {
 	 * @param this The class to create the logger for.
 	 */
 	inline fun createLogger(`this`: Any): Logger = createLogger(`this`::class.qualifiedName!!)
-
-	private fun createMdcProxy(logger: Logger): Logger = Proxy.newProxyInstance(
-		logger.javaClass.classLoader,
-		arrayOf(Logger::class.java)
-	) { _, method, args ->
-		val old = MDC.get(PREFIX_KEY)
-		try {
-			MDC.put(PREFIX_KEY, prefix)
-			method.invoke(logger, *args ?: emptyArray())
-		} finally {
-			if (old != null) MDC.put(PREFIX_KEY, old)
-			else MDC.remove(PREFIX_KEY)
-		}
-	} as Logger
-
-	companion object {
-		const val PREFIX_KEY = "prefix"
-	}
 }
