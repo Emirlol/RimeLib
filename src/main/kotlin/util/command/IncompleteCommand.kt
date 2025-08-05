6c15371f787e798c089df6b98f28b26a11c10d36
Rime <81419447+Emirlol@users.noreply.github.com>
2025-08-05 08:02:56 +0300

    Switch to existing exception type for IncompleteCommand.kt

--- src/main/kotlin/util/command/IncompleteCommand.kt
+++ src/main/kotlin/util/command/IncompleteCommand.kt
@@ -2,14 +2,15 @@ package me.ancientri.rimelib.util.command
 
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.context.CommandContext
+import com.mojang.brigadier.exceptions.CommandSyntaxException
 import net.minecraft.command.CommandSource
 
 object IncompleteCommand : Command<CommandSource> {
 	override fun run(context: CommandContext<CommandSource>): Int {
-		throw UnsupportedOperationException("This command is incomplete.")
+		throw CommandSyntaxException.BUILT_IN_EXCEPTIONS.dispatcherUnknownArgument().create()
 	}
 
-	fun <T: CommandSource> getInstance(): Command<T> {
+	fun <T : CommandSource> getInstance(): Command<T> {
 		@Suppress("UNCHECKED_CAST")
 		return this as Command<T>
 	}
