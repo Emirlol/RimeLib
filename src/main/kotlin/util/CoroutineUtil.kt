66b9ad0178b3773bc26b9305b56ac5ae21779cfc
Rime <81419447+Emirlol@users.noreply.github.com>
2025-08-05 08:03:37 +0300

    Remove event registering shenanigans

--- src/main/kotlin/util/CoroutineUtil.kt
+++ src/main/kotlin/util/CoroutineUtil.kt
@@ -2,15 +2,19 @@ package me.ancientri.rimelib.util
 
 import kotlinx.coroutines.*
 import me.ancientri.rimelib.RimeLib
-import me.ancientri.rimelib.util.events.Exitable
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
 import net.minecraft.client.MinecraftClient
 import org.slf4j.Logger
 
-object CoroutineUtil: Exitable() {
+object CoroutineUtil {
 	val globalJob = CoroutineScope(SupervisorJob() + Dispatchers.IO + CoroutineName(RimeLib.MOD_NAME))
 	private val LOGGER: Logger = RimeLib.loggerFactory.createLogger(this)
 
-	override fun exit(client: MinecraftClient) {
+	init {
+		ClientLifecycleEvents.CLIENT_STOPPING.register(::exit)
+	}
+
+	private fun exit(client: MinecraftClient) {
 		LOGGER.info("Cancelling all coroutines.")
 		globalJob.cancel()
 	}
