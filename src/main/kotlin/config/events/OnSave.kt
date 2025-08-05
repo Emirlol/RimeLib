fd3fb79f47b418ab5798f86d36a743b1205f5bb5
Rime <81419447+Emirlol@users.noreply.github.com>
2025-08-05 08:01:41 +0300

    Fix documentation

--- src/main/kotlin/config/events/OnSave.kt
+++ src/main/kotlin/config/events/OnSave.kt
@@ -1,9 +1,11 @@
 package me.ancientri.rimelib.config.events
 
+import me.ancientri.rimelib.config.ConfigManager
+
 /**
  * An interface for handling save events.
  *
- * @see ConfigManager#ON_SAVE
+ * @see ConfigManager.ON_SAVE
  */
 fun interface OnSave {
 	fun onSave()
