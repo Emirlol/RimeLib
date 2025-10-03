d127e79d5590206ae113acda4d0a736f533588a3
Rime <81419447+Emirlol@users.noreply.github.com>
2025-10-03 21:09:59 +0300

    Make library-specific field internal

--- src/main/kotlin/RimeLib.kt
+++ src/main/kotlin/RimeLib.kt
@@ -3,7 +3,6 @@ package me.ancientri.rimelib
 import me.ancientri.rimelib.util.CoroutineUtil
 import me.ancientri.rimelib.util.LoggerFactory
 import me.ancientri.rimelib.util.scheduler.Scheduler
-import net.minecraft.util.Identifier
 
 object RimeLib {
 	@Deprecated("This method is called by the Fabric Loader. Do not call this method manually.", level = DeprecationLevel.ERROR, replaceWith = ReplaceWith(""))
@@ -13,10 +12,8 @@ object RimeLib {
 		Scheduler
 	}
 
-	fun identifier(path: String): Identifier = Identifier.of(NAMESPACE, path)
+	internal const val MOD_NAME = "RimeLib"
+	internal const val NAMESPACE = "rimelib"
 
-	const val MOD_NAME = "RimeLib"
-	const val NAMESPACE = "rimelib"
-
-	val loggerFactory = LoggerFactory(MOD_NAME)
+	internal val loggerFactory = LoggerFactory(MOD_NAME)
 }
