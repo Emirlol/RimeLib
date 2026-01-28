cf92a8d304e183ad778e93e3e0d0be31be8496e3
Rime <81419447+Emirlol@users.noreply.github.com>
2026-01-28 22:21:58 +0300

    Add client coroutine dispatcher for convenience

--- src/main/kotlin/util/CoroutineUtil.kt
+++ src/main/kotlin/util/CoroutineUtil.kt
@@ -8,6 +8,18 @@ import org.slf4j.Logger
 
 object CoroutineUtil {
 	val globalJob = CoroutineScope(SupervisorJob() + Dispatchers.IO + CoroutineName(RimeLib.MOD_NAME))
+	/**
+	 * Client dispatcher for running tasks on the main Minecraft client thread.
+	 *
+	 * Example use:
+	 * ```kotlin
+	 * withContext(CoroutineUtil.clientDispatcher) {
+	 * 	// Code to run on the main Minecraft client thread
+	 * }
+	 * ```
+ 	 */
+	val clientDispatcher = client.asCoroutineDispatcher()
+
 	private val LOGGER: Logger = RimeLib.loggerFactory.createLogger(this)
 
 	init {
